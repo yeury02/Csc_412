473aad2494a340cb13e3f7028f1460787d41007e
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-10-06 19:56:24 -0400

    working on assignment 3

---
+++ assignment_3/program.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    // if there are not 4 arguments, can't compute anything
+    if (argc != 4)
+    {
+        printf("Invalid arguments.")
+    }
+    return 0;
+}
