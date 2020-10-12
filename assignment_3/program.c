e481adf7bc84f1ae0df7639a2939195af2102ffa
Yeury Galva Liriano <yeury02@my.uri.edu>
2020-10-11 21:35:38 -0400

    working on assignment

--- assignment_3/program.c
+++ assignment_3/program.c
@@ -33,65 +33,9 @@ int main(int argc, char* argv[])
     images = (Image*)malloc(sizeof(Image)*200);
     patterns = (Image*)malloc(sizeof(Image)*200);
 
-    read_dir(argv[1], images, patterns, &image_count, &pattern_count);
+    patterns[0] = read_image(argv[1]);
     read_dir(argv[2], images, patterns, &image_count, &pattern_count);
-}
-
-void read_dir(const char* dir_path, Image* images, Image* patterns, unsigned int* image_count, unsigned int* pattern_count)
-{
-    struct dirent *ep;              // Pointer for directory entry   
-    DIR *dp = opendir(dir_path);     // opendir() returns a pointer of DIR type.
-
-    if (dp != NULL)
-	{
-		char extension[100];       //file extensions will likely never be more than 100
-        while (ep = readdir(dp))
-        {   
-            printf("entry=%s\n", ep->d_name);
-
-            //check if this entry is a directory
-			if (ep->d_type == DT_DIR)
-			{
-				printf("    Entry is a directory\n");
-			}
-            //check if entry is a regular file
-            else if (ep->d_type == DT_REG)
-			{
-                char* new_path = (char*)malloc(sizeof(char) * (strlen(ep->d_name) + strlen(dir_path) + 2));
-                strcpy(new_path, dir_path);
-                strcat(new_path, "/");
-                strcat(new_path, ep->d_name);
-                printf("new path: %s", new_path);
-
-				//only check the extension of files not directories
-				get_extension(ep->d_name, extension);
-				printf("    extension=%s\n", extension);
-				
-				//use the extension to determine what type of file it is
-				if (strcmp(extension, ".img") == 0 && strcmp(extension, ".pat") == 0)
-				{
-                    Image tmp = read_image(new_path);
-                    if (tmp.numRows > 0 && tmp.numCols > 0)
-                    {
-                        if (strcmp(extension, ".img") == 0)
-                        {
-                            images[*image_count] = tmp;
-                            *image_count++;
-                        }
-                        else if (strcmp(extension, ".pat") == 0)
-                        {
-                            patterns[*pattern_count] = tmp;
-                            *pattern_count++;
-                        }
-                    }
-                    else 
-                    {
-                        printf("Could not load image file %s\n", new_path);
-                    }
-				}
-			}
-        }
-    }
+    //patterns[0] = read_image(argv[1]);
 }
 
 //get the extension and write to extension string
@@ -139,8 +83,8 @@ Image read_image(char* path)
         my_image.numRows = 0;
         return my_image;
     }
-    printf("Made it here\n");
-    fflush(stdout);
+    //printf("Made it here\n");
+    //fflush(stdout);
     // read dimensions
     fseek(img_file, 0, SEEK_SET);
     fscanf(img_file, "%u %u\n", &my_image.numCols, &my_image.numRows);
@@ -171,4 +115,115 @@ Image read_image(char* path)
     fclose(img_file);
     
     return my_image;
+}
+
+void read_dir(const char* dir_path, Image* images, Image* patterns, unsigned int* image_count, unsigned int* pattern_count)
+{
+    struct dirent *ep;              // Pointer for directory entry   
+    DIR *dp = opendir(dir_path);     // opendir() returns a pointer of DIR type.
+
+    if (dp != NULL)
+	{
+		char extension[100];       //file extensions will likely never be more than 100
+        while (ep = readdir(dp))
+        {   
+            //printf("entry=%s\n", ep->d_name);
+
+            //check if this entry is a directory
+			if (ep->d_type == DT_DIR)
+			{
+				//printf("    Entry is a directory\n");
+                continue;
+			}
+            //check if entry is a regular file
+            else if (ep->d_type == DT_REG)
+			{
+                char* new_path = (char*)malloc(sizeof(char) * (strlen(ep->d_name) + strlen(dir_path) + 2));
+                strcpy(new_path, dir_path);
+                strcat(new_path, "/");
+                strcat(new_path, ep->d_name);
+
+				//only check the extension of files not directories
+				get_extension(ep->d_name, extension);
+				//printf("    extension=%s\n", extension);
+                // printf("Made it here\n");
+                // fflush(stdout);
+				
+				//use the extension to determine what type of file it is
+                //|| strcmp(extension, ".pat") == 0)
+				if (strcmp(extension, ".img") == 0)
+				{
+                    Image tmp = read_image(new_path);
+                    print_matches(tmp, patterns);
+
+                    // 
+
+                    
+                    // as long as the file is not empty
+                    // if (tmp.numRows > 0 && tmp.numCols > 0)
+                    // {
+                    //     //printf("Made it here\n");
+                    //     //fflush(stdout);
+                    //     //printf("new path: %s\n", new_path);
+                    //     //images[*image_count] = tmp;
+                    //     *image_count++;
+                    //     printf("%ls", image_count);
+                    //     // else if (strcmp(extension, ".pat") == 0)
+                    //     // {
+                    //     //     patterns[*pattern_count] = tmp;
+                    //     //     *pattern_count++;
+                    //     // }
+                    // }
+                    // else
+                    // {
+                    //     printf("Could not load image file %s\n", new_path);
+                    // }
+				}
+                //   look for matches of pattern in temp
+
+                //  priduce output
+
+                //  free memory:  data in tmp  and new_path
+			}
+        }
+    }
+}
+
+void print_matches(Image* image, Image* pattern)
+{
+    // iterate overy every possile positio of patter in the image
+    for (unsigned int i=0; i<image->numRows-pattern->numRows; i++)
+    {
+        for (unsigned int j=0; j<image->numCols-pattern->numCols; j++)
+        {
+            // do we find the pattern at locationn (i,j) in the loop
+            int match = 1;
+            int count = 0;
+            // loop over the entire pattern until reach the end
+            for (unsigned int k=0; k<pattern->numRows; k++)
+            {
+                for (unsigned int l=0; l<pattern->numCols; l++)
+                {
+                    // compare
+                    if (pattern->data[k][l] == image->data[i+k][j+l])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        match = 0;
+                    }
+                }
+            if (match) 
+            {
+                count++;
+                // increment counter, store (i,j)
+                printf("matches: %d i:%d j:%d", count, i, j);
+
+            }
+            }
+        }
+       
+    }
+
 }
